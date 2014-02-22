a86c7a2429bafaa48c48597318e464148f62d56b
Machyne <mcotter222@gmail.com>
2014-02-22 11:49:50 -0600

    Add some shell structure

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Room(Base):
+    __tablename__ = 'rooms'
+
+    id = Column(Integer, primary_key=True)
+    hall = Column(String(60), unique=False)
+    draw_number = Column(Integer, unique=False)
+    room_number = Column(String(10), unique=False)
+    year = Column(Integer, unique=False)
+
+    def __init__(self, hall=None, draw_number=None, room_number=None, year=None):
+        self.hall = hall
+        self.draw_number = draw_number
+        self.room_number = room_number
+        self.year
+
+    def __repr__(self):
+        return '<Room: %r hall>' % (self.hall)
