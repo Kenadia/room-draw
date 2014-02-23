7fac460e5419a63f67fcc7f16e8599fc3f60b2a4
Ken Schiller <kenschiller@gmail.com>
2014-02-22 18:56:53 -0600

    Use draw order per dorm

--- seed_db.py
+++ seed_db.py
@@ -1,10 +1,12 @@
 from roomdraw import db
 from roomdraw import Room
+from collections import defaultdict
 
 
 if __name__ == '__main__':
     data = open("data.csv").read()
     lines = data.split("\r\n")[1:]
+    rooms = defaultdict(list)
     for l in lines:
         room, term, draw = l.split(',')
         if draw:  # Davis 007
@@ -12,6 +14,11 @@ if __name__ == '__main__':
             room_number = room.split()[1]
             draw_number = int(draw)
             year = int(term[:2]) + 2000
-            x = Room(hall, room_number, draw_number, year)
+            rooms[hall].append((hall, room_number, draw_number, year))
+    rooms = {hall:sorted(rooms[hall], key=lambda x: x[1]) for hall in rooms}
+    for hall in rooms:
+        for i, r in enumerate(rooms[hall]):
+            hall, room_number, draw_number, year = r
+            x = Room(hall, room_number, draw_number, i + 1, year)
             db.session.add(x)
     db.session.commit()
